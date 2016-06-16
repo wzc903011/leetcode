bc10aec28c9e5c83d7b2a4ba2e5ca1b892306564
zhicheng.wang <wzc903011@163.com>
2016-06-16 16:32:37 +0800

    modified:   Intersection of Two Arrays II/Intersection of Two Arrays II.java

--- Intersection of Two Arrays II/Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II/Intersection of Two Arrays II.java
@@ -10,8 +10,9 @@ import java.util.Set;
     
     public static int[] hashMap(int[] nums1, int[] nums2){
     	HashMap<Integer,Integer> num1_map = new HashMap<Integer,Integer>();
-    	HashMap<Integer,Integer> num2_map = new HashMap<Integer,Integer>();
     	List<Integer> array = new ArrayList<Integer>();
+
+	// add nums1 to hashmap
     	for(int i=0; i<nums1.length; i++) {
     		if(num1_map.containsKey(nums1[i])){
     			int count = num1_map.get(nums1[i]);
@@ -22,6 +23,7 @@ import java.util.Set;
     		}
     	}
     	
+	// iterate nums2 
     	for(int i=0; i< nums2.length; i++) { 
     		if(num1_map.get(nums2[i]) != null && num1_map.get(nums2[i]) != 0 ) {
     			int count = num1_map.get(nums2[i]);
