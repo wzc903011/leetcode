21268467f6f2be6d046784855b2262b11485a45d
wzc903011 <listener.wang@gmail.com>
2016-06-16 16:35:00 +0800

    Update Intersection of Two Arrays II.java

--- Intersection of Two Arrays II/Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II/Intersection of Two Arrays II.java
@@ -1,11 +1,6 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
+/*
+*  use hashMap to save nums1
+*/
  class Solution {
     
     public static int[] hashMap(int[] nums1, int[] nums2){
