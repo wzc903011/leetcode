5b86c4be1fda30eb808dd79bedd21871c36e3d51
wzc903011 <listener.wang@gmail.com>
2016-09-30 11:23:20 +0800

    Update Add Digits.java

--- Add Digits/Add Digits.java
+++ Add Digits/Add Digits.java
@@ -1,3 +1,7 @@
+/*
+ *  n = d1 + 10d2 + 100d3 + .... + 10...dm     n % 9 = (d1 + d2 + d3 +....+ dm) % 9;   
+ *  n = 9m --- 
+ */
 public class Solution {
      public int addDigits(int num) {
      	return (num -1) % 9 + 1;
