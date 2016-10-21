f24ca456779fbfd500911c37270e7310820c814e
wzc903011 <listener.wang@gmail.com>
2016-10-21 21:49:17 +0800

    Update Reverse String.java

--- Reverse String/Reverse String.java
+++ Reverse String/Reverse String.java
@@ -1,3 +1,11 @@
+/*
+* Write a function that takes a string as input and returns the string reversed.
+
+* Example:
+* Given s = "hello", return "olleh".
+
+* Subscribe to see which companies asked this question
+*/
 public class Solution {
     public String reverseString(String s) {
         StringBuilder strBuilder = new StringBuilder(s);
