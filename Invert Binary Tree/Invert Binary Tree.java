ded0af6e83f9186f8afdaa9906366e7a549f975b
zhicheng.wang <wzc903011@163.com>
2016-10-22 16:59:40 +0800

    a

---
+++ Invert Binary Tree/Invert Binary Tree.java
@@ -0,0 +1,20 @@
+/**  ab | cd to  dc | ba  ==   sideInvert + sideSwap
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)  return root;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
