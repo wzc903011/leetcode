d2225fccaba73fd2dcf815d8d983a8f5c2cfab7f
zhicheng.wang <wzc903011@163.com>
2016-10-22 17:29:51 +0800

    digui

---
+++ Same Tree/Same Tree.java
@@ -0,0 +1,20 @@
+/* Given two binary trees, write a function to check if they are equal or not.
+ *Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && p ==  q) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return left&right;
+    }
+}
