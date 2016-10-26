858c27b2544c2b6fd3bda92b55811d06c73f5e56
zhicheng.wang <wzc903011@163.com>
2016-10-26 23:12:05 +0800

    85%

--- First Unique Character in a String/First Unique Character in a String.java
+++ First Unique Character in a String/First Unique Character in a String.java
@@ -1,5 +1,32 @@
 //Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+// You may assume the string contain only lowercase letters.
 
+public class Solution {
+    public int firstUniqChar(String s) {
+        int result = -1;
+        int length = s.length();
+        if (length == 0) return result;
+        if (length == 1) return 0;
+        int[] chars = new int[26];
+        for(int i = 0; i < length; i++) {
+            final char c = s.charAt(i);
+            chars[c - 'a'] += 1;
+        }
+        
+        for(int i = 0; i < length; i++) {
+            final char c = s.charAt(i);
+            if(chars[c -'a'] == 1) 
+            {
+                result = i;
+            return result;
+            }
+        }
+        return result;
+}
+}
+
+ 
+//  if not only lowercase letters
 public class Solution {
     public int firstUniqChar(String s) {
         int result = -1;
