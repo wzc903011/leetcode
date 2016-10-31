547bf4072620abde628497942e99d58d1b20d9a7
zhicheng.wang <wzc903011@163.com>
2016-10-31 23:03:21 +0800

    sort make it 85%

--- Contains Duplicate/Contains Duplicate.java
+++ Contains Duplicate/Contains Duplicate.java
@@ -15,3 +15,18 @@ public class Solution {
         return false;
     }
 }
+
+
+//   85%  --- related to the TestCase
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[i - 1]) return true;
+        }
+        return false;
+    }
+}
+
+
+
